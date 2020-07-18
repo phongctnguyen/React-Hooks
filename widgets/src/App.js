03d2c4e42430a9d588cff0fb3be3302a7b2e2b85
Nguyen Cao Truong Phong <phongctnguyen@gmail.com>
2020-07-19 02:11:23 +0700

    create acccordion component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
+import Accordion from './component/Accordion';
+
+const items = [
+	{
+		title: 'What is React?',
+		content: 'React is a front end javascript framework',
+	},
+	{
+		title: 'Why use React?',
+		content: 'React is a favorite JS library among engineers',
+	},
+	{
+		title: 'How do you use React?',
+		content: 'You use React by creating components',
+	},
+]
+
 export default () => {
-  return <h1>Widgets App</h1>
+  return <Accordion items={items} />
 };
---
+++ widgets/src/component/Accordion.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default ({items}) => {
+  return <h1>{items.length}</h1>
+};
