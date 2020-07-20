521fc6f1d03090601861025e81aa55dfece1456c
phongctnguyen <phongctnguyen@gmail.com>
2020-07-20 22:30:02 +0700

    optimize useEffect in Search component

--- widgets/src/component/Search.js
+++ widgets/src/component/Search.js
@@ -3,7 +3,18 @@ import axios from 'axios';
 
 export default () => {
   const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000)
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -13,7 +24,7 @@ export default () => {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term,
+          srsearch: debouncedTerm,
         }
       });
 
@@ -21,8 +32,27 @@ export default () => {
     };
 
     search();
-  }, [term]);
+  }, [debouncedTerm])
 
+  const renderedResults = results.map(result => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}>
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    )
+  })
 
   return (
     <div>
@@ -34,6 +64,9 @@ export default () => {
             value={term}
             onChange={e => setTerm(e.target.value)} />
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   )
