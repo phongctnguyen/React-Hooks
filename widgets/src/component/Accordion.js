20f920b7b49886c5c5499e69d1a257d64d00b16b
Nguyen Cao Truong Phong <phongctnguyen@gmail.com>
2020-07-23 10:36:48 +0700

    add feature on/off of DOM itself in accordion component

--- widgets/src/component/Accordion.js
+++ widgets/src/component/Accordion.js
@@ -2,20 +2,25 @@ import React, { useState } from 'react';
 
 export default ({ items }) => {
   const [activeIndex, setActiveIndex] = useState(null);
+  const [open, setOpen] = useState(false);
 
-  const onTitleClick = index => {
+  const onTitleClick = (index) => {
+    if (activeIndex !== index) {
+      setOpen(true);
+    } else {
+      setOpen(!open)
+    }
+
     setActiveIndex(index);
   }
 
   const renderedItems = items.map((item, index) => {
-    const active = index === activeIndex ? 'active' : '';
+    const active = open && activeIndex === index ? 'active' : '';
 
     return (
       <React.Fragment key={item.title}>
-        <div
-          className={`title ${active}`}
-          onClick={() => onTitleClick(index)}>
-          <i className="dropdown icon"></i>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+          <i className='dropdown icon'></i>
           {item.title}
         </div>
         <div className={`content ${active}`}>
@@ -23,11 +28,7 @@ export default ({ items }) => {
         </div>
       </React.Fragment>
     )
-  })
+  });
 
-  return (
-    <div className="ui styled accordion">
-      {renderedItems}
-    </div>
-  )
+  return <div className='ui styled accordion'>{renderedItems}</div>
 };
